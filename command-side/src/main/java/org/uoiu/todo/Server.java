51e7831ccb9bd7e66336be5022595481b473a64a
悾 <xxxkonyi@gmail.com>
2016-08-12 10:35:13 +0800

    增加日志，HTTP请求日志，HTTP响应时间

--- command-side/src/main/java/org/uoiu/todo/Server.java
+++ command-side/src/main/java/org/uoiu/todo/Server.java
@@ -1,39 +1,69 @@
 package org.uoiu.todo;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.core.logging.SLF4JLogDelegateFactory;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.CorsHandler;
+import io.vertx.ext.web.handler.LoggerHandler;
+import io.vertx.ext.web.handler.ResponseTimeHandler;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
 public class Server extends AbstractVerticle {
 
+  static {
+    System.setProperty(LoggerFactory.LOGGER_DELEGATE_FACTORY_CLASS_NAME, SLF4JLogDelegateFactory.class.getName());
+  }
+
+  private static final Logger log = LoggerFactory.getLogger(Server.class);
+
+  private final static int PORT = 8082;
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(Server.class.getName());
+  }
+
   @Override
   public void start() throws Exception {
+    log.info("Starting HTTP server for vert.x web application");
 
     Router router = Router.router(vertx);
 
-    router.route().handler(CorsHandler.create("*")
+    router.route().handler(LoggerHandler.create());
+    router.route().handler(ResponseTimeHandler.create());
+
+    router.route().path("/").handler(request -> request.response().end("Hello Vertx Chat Event Bus !"));
+
+    router.route("/eventbus/*").handler(CorsHandler.create("*")
       .allowedMethod(HttpMethod.GET)
       .allowedMethod(HttpMethod.POST)
+      .allowedMethod(HttpMethod.PUT)
+      .allowedMethod(HttpMethod.DELETE)
       .allowedMethod(HttpMethod.OPTIONS)
       .allowedHeader("X-PINGARUNER")
       .allowedHeader("Content-Type"));
 
 
     // Allow events for the designated addresses in/out of the event bus bridge
-    BridgeOptions opts = new BridgeOptions()
+    BridgeOptions bridgeOptions = new BridgeOptions()
       .addInboundPermitted(new PermittedOptions().setAddress("chat.message"))
       .addOutboundPermitted(new PermittedOptions().setAddress("chat.message"));
 
     // Create the event bus bridge and add it to the router.
-    SockJSHandler ebHandler = SockJSHandler.create(vertx).bridge(opts);
-    router.route("/eventbus/*").handler(ebHandler);
+    SockJSHandler sockJSHandler = SockJSHandler.create(vertx).bridge(bridgeOptions);
+    router.route("/eventbus/*")
+      .handler(sockJSHandler);
 
     // Start the web server and tell it to use the router to handle requests.
-    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+    vertx.createHttpServer().requestHandler(router::accept).listen(PORT);
+
+    log.info("Started on port(s): {} (http)", String.valueOf(PORT));
 
   }
 }
