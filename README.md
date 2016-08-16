0e54e773d77f9a3e42182201a5eeadb5922e4489
悾 <xxxkonyi@gmail.com>
2016-08-16 17:43:11 +0800

    webpack 安装环境启动示例

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # todo
 todo demo
+
+
+app
+npm install webpack -g
+npm install webpack-dev-server -g
+webpack
+npm start
---
+++ app/webpack.config.js
@@ -0,0 +1,12 @@
+var webpack = require('webpack');
+var path = require("path");
+
+module.exports = {
+  entry: {
+    app: ["./app/main.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  }
+};
